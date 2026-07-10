9ac1902650cdbda88b90fb901e880f3029cc297a
RiccardoAccatino <20054233@studenti.uniupo.it>
2026-07-10 16:24:26 +0200

    TypeChecking completo con tutti

---
+++ src/test/TestTypeChecking.java
@@ -0,0 +1,126 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import ast.NodeProgram;
+import parser.Parser;
+import scanner.Scanner;
+import symbolTable.SymbolTable;
+import visitor.TipoTD;
+import visitor.TypeCheckingVisitor;
+import visitor.TypeDescriptor;
+
+public class TestTypeChecking {
+
+    // --- TEST CON ERRORI SEMANTICI (Ci aspettiamo TipoTD.ERROR) ---
+
+    @Test
+    public void testDicRipetute() throws Exception {
+        SymbolTable.init();
+        Scanner scanner = new Scanner("src/test/data/testTypeChecking/1_dicRipetute.txt");
+        Parser parser = new Parser(scanner);
+        NodeProgram nP = parser.parse();
+
+        TypeCheckingVisitor tcVisit = new TypeCheckingVisitor();
+        nP.accept(tcVisit);
+        TypeDescriptor res = tcVisit.getResType();
+
+        // Ci aspettiamo un errore perché la variabile 'a' viene dichiarata due volte
+        assertEquals(TipoTD.ERROR, res.getTipo(), "Dovrebbe dare errore per dichiarazione ripetuta");
+    }
+
+    @Test
+    public void testIdNonDec1() throws Exception {
+        SymbolTable.init();
+        Scanner scanner = new Scanner("src/test/data/testTypeChecking/2_idNonDec.txt");
+        Parser parser = new Parser(scanner);
+        NodeProgram nP = parser.parse();
+
+        TypeCheckingVisitor tcVisit = new TypeCheckingVisitor();
+        nP.accept(tcVisit);
+        TypeDescriptor res = tcVisit.getResType();
+
+        // Ci aspettiamo un errore perché viene stampata la variabile 'b' mai dichiarata
+        assertEquals(TipoTD.ERROR, res.getTipo(), "Dovrebbe dare errore per variabile non dichiarata nella print");
+    }
+
+    @Test
+    public void testIdNonDec2() throws Exception {
+        SymbolTable.init();
+        // NOTA: Usa "3_idNonDec" se il file non ha l'estensione .txt, altrimenti aggiungila qui
+        Scanner scanner = new Scanner("src/test/data/testTypeChecking/3_idNonDec"); 
+        Parser parser = new Parser(scanner);
+        NodeProgram nP = parser.parse();
+
+        TypeCheckingVisitor tcVisit = new TypeCheckingVisitor();
+        nP.accept(tcVisit);
+        TypeDescriptor res = tcVisit.getResType();
+
+        // Ci aspettiamo un errore perché nell'espressione si usa la variabile 'c' mai dichiarata
+        assertEquals(TipoTD.ERROR, res.getTipo(), "Dovrebbe dare errore per variabile 'c' usata ma non dichiarata");
+    }
+
+    @Test
+    public void testTipoNonCompatibile() throws Exception {
+        SymbolTable.init();
+        Scanner scanner = new Scanner("src/test/data/testTypeChecking/4_tipoNonCompatibile.txt");
+        Parser parser = new Parser(scanner);
+        NodeProgram nP = parser.parse();
+
+        TypeCheckingVisitor tcVisit = new TypeCheckingVisitor();
+        nP.accept(tcVisit);
+        TypeDescriptor res = tcVisit.getResType();
+
+        // Ci aspettiamo un errore perché si cerca di assegnare un float (0.1) a un int (b)
+        assertEquals(TipoTD.ERROR, res.getTipo(), "Dovrebbe dare errore di incompatibilità di tipo");
+    }
+
+    // --- TEST CORRETTI (Ci aspettiamo TipoTD.OK) ---
+
+    @Test
+    public void testCorretto1() throws Exception {
+        SymbolTable.init();
+        Scanner scanner = new Scanner("src/test/data/testTypeChecking/5_corretto.txt");
+        Parser parser = new Parser(scanner);
+        NodeProgram nP = parser.parse();
+
+        TypeCheckingVisitor tcVisit = new TypeCheckingVisitor();
+        nP.accept(tcVisit);
+        TypeDescriptor res = tcVisit.getResType();
+
+        // Codice semanticamente corretto
+        assertEquals(TipoTD.OK, res.getTipo(), "Il Type Checking dovrebbe dare esito OK. Errore: " + res.getMsg());
+    }
+
+    @Test
+    public void testCorretto2() throws Exception {
+        SymbolTable.init();
+        Scanner scanner = new Scanner("src/test/data/testTypeChecking/6_corretto.txt");
+        Parser parser = new Parser(scanner);
+        NodeProgram nP = parser.parse();
+
+        TypeCheckingVisitor tcVisit = new TypeCheckingVisitor();
+        nP.accept(tcVisit);
+        TypeDescriptor res = tcVisit.getResType();
+
+        // Codice semanticamente corretto con espressioni miste int/float
+        assertEquals(TipoTD.OK, res.getTipo(), "Il Type Checking dovrebbe dare esito OK. Errore: " + res.getMsg());
+    }
+
+    @Test
+    public void testCorretto3() throws Exception {
+        SymbolTable.init();
+        Scanner scanner = new Scanner("src/test/data/testTypeChecking/7_corretto.txt");
+        Parser parser = new Parser(scanner);
+        NodeProgram nP = parser.parse();
+
+        TypeCheckingVisitor tcVisit = new TypeCheckingVisitor();
+        nP.accept(tcVisit);
+        TypeDescriptor res = tcVisit.getResType();
+
+        // Codice semanticamente corretto con operatore -=
+        assertEquals(TipoTD.OK, res.getTipo(), "Il Type Checking dovrebbe dare esito OK. Errore: " + res.getMsg());
+    }
+}
